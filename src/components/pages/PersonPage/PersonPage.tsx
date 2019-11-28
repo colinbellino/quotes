654b11bd0766831aa1ec9e242c5401d4c381e00a
Colin Bellino <bellino.colin@gmail.com>
2019-11-28 14:23:13 +0100

    🐛 Fix PersonPage after data change

--- src/components/pages/PersonPage/PersonPage.tsx
+++ src/components/pages/PersonPage/PersonPage.tsx
@@ -2,53 +2,60 @@ import React, { FunctionComponent } from "react";
 import { RouteComponentProps } from "@reach/router";
 import useFetch from "fetch-suspense";
 
-import { persons, Person, Quote as QuoteModel } from "data";
+import { Person as PersonModel, Quote as QuoteModel } from "data";
 import { Avatar, Quote } from "components";
 import { QUOTES_URL } from "config";
 import "./PersonPage.css";
 
-type EnhancedPerson = Person & { id: string };
+type EnhancedPerson = PersonModel & { id: string };
 
 type PersonPageViewProps = {
-  person: EnhancedPerson;
+  person?: EnhancedPerson;
   quotes?: QuoteModel[];
 };
 
 export const PersonPageView = ({
   person,
   quotes = [],
-}: PersonPageViewProps) => (
-  <main className="PersonPage">
-    <section className="PersonCard">
-      <Avatar
-        color={person.color}
-        url={person.avatar}
-        alt={`${person.id}'s avatar`}
-      />
-      <div className="Info">
-        <h2>{person.id}</h2>
-        <h3>{`${quotes.length} quote${quotes.length > 1 ? "s" : ""}`}</h3>
-      </div>
-      <ul>
-        {quotes.map(quote => (
-          <Quote key={quote.id} quote={quote} />
-        ))}
-      </ul>
-    </section>
-  </main>
-);
+}: PersonPageViewProps) => {
+  if (!person) {
+    return null;
+  }
+
+  return (
+    <main className="PersonPage">
+      <section className="PersonCard">
+        <Avatar
+          color={person.color}
+          url={person.avatar}
+          alt={`${person.id}'s avatar`}
+        />
+        <div className="Info">
+          <h2>{person.id}</h2>
+          <h3>{`${quotes.length} quote${quotes.length > 1 ? "s" : ""}`}</h3>
+        </div>
+        <ul>
+          {quotes.map(quote => (
+            <Quote key={quote.id} quote={quote} />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+};
 
 export const PersonPage: FunctionComponent<RouteComponentProps<{
   id: string;
 }>> = ({ id }) => {
-  const person = persons.find(data => data.id === id) || {
-    id: id!,
-    avatar: "",
-    color: "",
+  const {
+    data: { persons, quotes },
+  } = useFetch(QUOTES_URL) as {
+    data: {
+      quotes: QuoteModel[];
+      persons: PersonModel[];
+    };
   };
-  const { data: quotes } = useFetch(QUOTES_URL) as {
-    data: QuoteModel[];
-  };
+  const person = persons.find(data => data.id === id);
   const filteredQuotes = quotes.filter(quote => quote.author === id);
 
   return PersonPageView({ person, quotes: filteredQuotes });
