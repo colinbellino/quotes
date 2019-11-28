bbb20d032e251d8c742228ddc5ce2ac66068fdbd
Colin Bellino <bellino.colin@gmail.com>
2019-11-28 11:41:17 +0100

    ✨Add Ricardo and Emmanuel to the team

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -13,56 +13,61 @@ export const persons: Person[] = [
   {
     id: "Cédric",
     color: "#F50057",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UA7TTD0BD-ga403e40113b-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UA7TTD0BD-ga403e40113b-120",
   },
   {
     id: "Colin",
     color: "#651FFF",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UD0GHUHBR-d5960196159c-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UD0GHUHBR-d5960196159c-120",
   },
   {
     id: "David",
     color: "#3D5AFE",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UH25TVDB4-8a8b8bb6b26e-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UH25TVDB4-8a8b8bb6b26e-120",
   },
   {
     id: "Julie",
     color: "#42A5F5",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-U830H23DJ-c34ef65daf1f-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-U830H23DJ-c34ef65daf1f-120",
   },
   {
     id: "Julien",
     color: "#26A69A",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UF7ALEVQQ-g8913afed5a7-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UF7ALEVQQ-g8913afed5a7-120",
   },
   {
     id: "Rudy",
     color: "#8BC34A",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-U9VC2SD2M-337efcaf401d-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-U9VC2SD2M-337efcaf401d-120",
   },
   {
     id: "Valérie",
     color: "#546E7A",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UGMH3BV1V-g14ffeaeddec-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UGMH3BV1V-g14ffeaeddec-120",
   },
   {
     id: "Wolfgang",
     color: "#8D6E63",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UCL8TF62H-350ba56bac47-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UCL8TF62H-350ba56bac47-120",
   },
   {
     id: "Thaïs",
     color: "#FF9100",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UCB0RPYRX-88269b24a0f5-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UCB0RPYRX-88269b24a0f5-120",
   },
   {
-    id: "Marwen",
+    id: "Ricardo",
     color: "#78909C",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UA00T5UH2-abe22c72e78e-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UMMEHMBD2-0f774b706011-120",
   },
   {
     id: "Céline",
     color: "#146616",
-    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UM5RS7BDL-39ef555ff6e6-72",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UM5RS7BDL-39ef555ff6e6-120",
+  },
+  {
+    id: "Emmanuel",
+    color: "#2D334A",
+    avatar: "https://ca.slack-edge.com/T6K4Y3HD2-UQRG6JHKN-bef37b678641-120",
   },
 ];
