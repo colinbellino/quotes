2dd431f4601ac4f5ce054cd375add875ce186922
Colin Bellino <bellino.colin@gmail.com>
2021-01-31 03:36:02 +0100

    🐛 Replace line break in cert

--- src/pages/api/quotes.ts
+++ src/pages/api/quotes.ts
@@ -41,7 +41,7 @@ export async function loadDoc() {
   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_DOCUMENT_ID!);
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!,
-    private_key: process.env.GOOGLE_PRIVATE_KEY!,
+    private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/gm, "\n"),
   });
 
   await doc.loadInfo();
